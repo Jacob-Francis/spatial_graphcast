76f5ca27ac0ca939dc8a4b2071958e33c424a31a
jacob <jjf817@ic.ac.uk>
2025-05-23 12:00:31 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="custom_loss",
+    version="0.1",
+    packages=find_packages(),
+    package_data={"custom_loss": ["*.so"]},  # include .so files in the package
+    include_package_data=True,
+)
